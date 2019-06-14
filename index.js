9112b55f8d14d8e04b7b6475dfc955d9553b2acb
Ritwik <ritwikshanker@gmail.com>
2019-06-14 11:59:31 +0530

    cleaned code

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app);
-console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+// console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
