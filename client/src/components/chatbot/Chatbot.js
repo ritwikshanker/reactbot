97d30869d0090237a25cfbf112333b3f23cad237
taran1515 <singhtaran1515@gmail.com>
2019-06-19 17:25:27 +0530

    Half Frontend build

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,10 +1,98 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios/index';
+import Message from './Message';
 
-const Chatbot = () =>
-{
-    return (
-        <h2> Chatbot will be here</h2>
-    )
-};
+class Chatbot extends Component {
+    messagesEnd;
+    constructor(props){
+        super(props);
+
+        this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+        this.state= {
+            messages:[]
+        }
+    }
+
+    async df_text_query(text){
+        let says = {
+            speaks:'me',
+            msg:{
+                text:{
+                    text:text
+                }
+            }
+        };
+
+        this.setState({messages: [...this.state.message,says]});
+
+        const res = await axios.post('/api/df_text_query',{text});
+
+        for(let msg of res.data.fulfillmentMessages){
+            says = {
+                speaks:'bot',
+                msg:msg
+            };
+            this.setState({messages: [...this.state.message,says]});
+        }
+
+    }
+
+    async df_event_query(event){
+        const res = await axios.post('/api/df_event_query',{event});
+
+        for(let msg of res.data.fulfillmentMessages){
+            let says = {
+                speaks:'me',
+                msg:msg
+            };
+            this.setState({message: [...this.state.message,says]});
+        }
+
+    }
+
+    componentDidMount() {
+        this.df_event_query('Welcome');
+    }
+
+    componentDidUpdate() {
+        this.messagesEnd.scrollIntoView({behavior:"smooth"})
+
+
+    }
+
+    renderMessages(stateMessages){
+        if(stateMessages){
+            return stateMessages.map((message,i) =>{
+                return <Message speaks={message.speaks} text={message.msg.text.text }/>;
+            });
+
+        }else {
+            return null;
+        }
+
+    }
+
+    _handleInputKeyPress(e){
+        if(e.keyb==='Enter'){
+            this.df_event_query(e.target.value);
+            e.target.value = '';
+        }
+    }
+
+    render() {
+        return(<div style={{height: 400, width: 400, float: 'right'}}>
+            <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
+                <h2>Chatbot</h2>
+                {this.renderMessages(this.state.messages)}
+                <div ref={(el) => {this.messagesEnd = el;}}
+                    style={{float:'left',clear:"both"}}></div>
+                <input type="text" onKeyPress={this._handleInputKeyPress}/>
+            </div>
+        </div>)
+    }
+    }
+
+
+
 
 export default Chatbot;
