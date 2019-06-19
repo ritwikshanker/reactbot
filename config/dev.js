030e1d8f555a15d5c8fcf0a8f65683c7a68e446b
Ritwik Shanker <ritwikshanker@yahoo.in>
2019-06-19 18:18:43 +0530

    Clean up ignored files

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleProjectID: 'petpalagent-jiiiks',
-    dialogFlowSessionID: 'react-bot-session',
-    dialogFlowSessionLanguageCode: 'en-US',
-    googleClientEmail: 'https://console.cloud.google.com/iam-admin/serviceaccounts/project?project=petpalagent-naygls',
-    mongoURI: "mongodb://localhost:27017/ChatBot_training"
-};
