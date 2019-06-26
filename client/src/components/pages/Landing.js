12c2d95734aab2ff872cc20726c37bac39f43ec0
taran1515 <singhtaran1515@gmail.com>
2019-06-26 12:50:08 +0530

    Icons Added

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import { IconContext } from "react-icons";
+import { FaPhone } from 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa';
+import { FaRocketchat } from 'react-icons/fa';
+import {Link} from "react-router-dom";
 const Landing = () => (
+
     <div style={{textAlign: 'center'}}>
-        <h1>Selling you great stuff!</h1>
-        with the help of the chatbot
+        <h3>Selling you great stuff!</h3>
+        with the help of the chatbot<br/><br/><br/>
+        <ul id="nav-mobile" className="left hide-on-med-and-down" style={{border:"2px solid black",borderRadius:'30%',padding:'5px',margin:'5px'}}>
+        <li><Link to={'/shop'}>
+        <IconContext.Provider value={{ color: "black", className: "global-class-name", size:"3em" ,textAlign: 'right'}}>
+            <div>
+                <FaPhone/><br/>
+
+            </div>
+        </IconContext.Provider></Link></li></ul>
+        <ul id="nav-mobile" className="left hide-on-med-and-down" style={{border:"2px solid black",borderRadius:'30%',padding:'5px',margin:'5px'}}>
+        <li><Link to={'/shop'}>
+            <IconContext.Provider value={{ color: "black", className: "global-class-name", size:"3em" ,textAlign: 'right'}}>
+                <div>
+                    <FaWhatsapp/><br/>
+
+                </div>
+            </IconContext.Provider></Link></li></ul>
+        <ul id="nav-mobile" className="left hide-on-med-and-down" style={{border:"2px solid black",borderRadius:'30%',padding:'5px',margin:'5px'}}>
+        <li><Link to={'/'}>
+            <IconContext.Provider value={{ color: "black", className: "global-class-name", size:"3em" ,textAlign: 'right'}}>
+                <div>
+                    <FaRocketchat/><br/>
+
+                </div>
+            </IconContext.Provider></Link></li></ul>
     </div>
 );
 
